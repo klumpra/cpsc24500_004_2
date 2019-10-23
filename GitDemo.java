6a80d5a7e8fbd879cb54580d1e718b0bca85ad22
Dr. Ray Klump <klumpra@lewisu.edu>
2019-10-22 20:17:38 -0500

    Development branch first commit.

--- GitDemo.java
+++ GitDemo.java
@@ -3,5 +3,6 @@ public class GitDemo {
 		System.out.println("Hello World.");
 		System.out.println("Check me out!");
 		System.out.println("This is uproariously cool.");
+		System.out.println("I did this on the dev branch.");
 	}
 }
